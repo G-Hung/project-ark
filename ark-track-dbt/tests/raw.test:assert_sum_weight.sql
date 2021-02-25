852f56da2311763608015787c1a0c47109a41f81
G-Hung <geoffrey12177@gmail.com>
2021-02-24 17:46:21 -0800

    change severity to warn, val < 99 is possible

--- ark-track-dbt/tests/raw.test:assert_sum_weight.sql
+++ ark-track-dbt/tests/raw.test:assert_sum_weight.sql
@@ -1,5 +1,12 @@
 -- If this fails: it means sum of weight is less than 99% for a specific date and fund
 
+-- warning instead, it is possible the sum < 99, eg on 24 Feb 2021
+{{
+    config(
+        severity='warn'
+    )
+}}
+
 SELECT date, fund, SUM(weight) AS total_weight
 FROM {{ source('raw', 'test') }}
 GROUP BY 1, 2
