d78d3cc82c0470ca6a9dd59a54b48a4ec78e9888
G-Hung <geoffrey12177@gmail.com>
2021-12-20 22:30:41 -0800

    add dbt tests to prevent no new data

---
+++ ark-track-dbt/tests/raw.test:assert_discontinue_date_less_than_3.sql
@@ -0,0 +1,10 @@
+-- If this fails: date diff b/w current_date and current max_date is larger than 2, aka: we don't have new data for 3+ days, 3 is for weekend
+
+WITH base AS (
+    SELECT max(date) as max_date
+    FROM {{ source('raw', 'test') }}
+)
+
+SELECT *, CURRENT_DATE()
+FROM base
+WHERE DATE_DIFF(CURRENT_DATE(), max_date, DAY) > 2
