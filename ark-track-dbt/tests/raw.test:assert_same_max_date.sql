b7a51a4db216e2a2a29adfd28159caee73af9ab5
G-Hung <geoffrey12177@gmail.com>
2022-08-01 21:37:09 -0700

    fix: remove CTRU

--- ark-track-dbt/tests/raw.test:assert_same_max_date.sql
+++ ark-track-dbt/tests/raw.test:assert_same_max_date.sql
@@ -1,8 +1,11 @@
--- If this fails: the max date are different, aka: some funds are not updated
+-- If this fails: the max date are different, ie: some funds are not updated
 
 WITH base AS (
     SELECT fund, max(date) AS max_date
     FROM {{ source('raw', 'test') }}
+    -- CTRU is closed
+    -- ref: https://www.prnewswire.com/news-releases/ark-investment-management-llc-announces-it-will-close-the-ark-transparency-etf-the-fund-ticker-ctru-301589562.html
+    WHERE fund != 'CTRU'
     GROUP BY 1
 )
 
